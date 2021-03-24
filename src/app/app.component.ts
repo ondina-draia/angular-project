ea8b444ec1585a8097a9b00099b58921257dee08
Ondina Draia-Nicolau <ondina.DRAIA-NICOLAU@etu.univ-amu.fr>
2021-03-24 11:17:11 +0100

    le programme recompile, tjrs un souci avec async

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppareilService } from "./services/appareil.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-project';
+
+export class AppComponent{
+  constructor() {}
 }
